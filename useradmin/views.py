f47c2bcdcc19a74c9e56800b4b89a5373e791454
蒋亮 <jliang@waterworld.com.cn>
2022-04-23 08:15:41 -0700

    增加一个登录页面useradmin/login,创建html页面->view.py定义方法处理页面->应用路由配置view的路由->主路由配置应用路由

--- useradmin/views.py
+++ useradmin/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def login_view(request):
+    if request.method=='GET':
+        return render(request,'useradmin/login.html')
