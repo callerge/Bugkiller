5562969a2c6be339a974473cfa9b044859517965
蒋亮 <jliang@waterworld.com.cn>
2022-04-24 06:56:16 -0700

    添加登录界面,实现用户秘密校验

--- useradmin/views.py
+++ useradmin/views.py
@@ -1,8 +1,29 @@
-from django.http import HttpResponse
+from django.contrib.auth import login,logout,authenticate
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
-
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from . import models
 # Create your views here.
 def login_view(request):
     if request.method=='GET':
         return render(request,'useradmin/login.html')
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user=authenticate(username=username,password=password)
+        if not user:
+            return HttpResponse("--用户名或密码错误--")
+        else:
+            login(request,user)
+            return HttpResponse("--成功--")
+            # return HttpResponseRedirect('/index')
+        pass
+#
+# @login_required
+# def index_view(request):
+#     #未登录则跳转至settings.LOGIN_URL
+#     user=request.user
+#     return HttpResponse('欢迎 %s'%(user.username))
+
+
