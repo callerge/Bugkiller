5aa07257202cd5d4af2a0015a33bc7e0168ed6f2
蒋亮 <jliang@waterworld.com.cn>
2022-06-04 10:49:33 -0700

    修改主页布局

--- Bugkiller/settings.py
+++ Bugkiller/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =(os.path.join(BASE_DIR,'static'),)
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+)
 #未登录情况下的跳转地址
 LOGIN_URL = '/useradmin/login'
