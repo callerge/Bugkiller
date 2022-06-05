23024d31d8eb612261f084e1926929d41a764113
蒋亮 <jliang@waterworld.com.cn>
2022-06-04 21:29:01 -0700

    Killer:模拟从数据库获取到列表，将左侧的模块按单选框的形式显示

--- killer/views.py
+++ killer/views.py
@@ -8,4 +8,5 @@ from django.http import HttpResponse
 def killer_view(request):
     user = request.user
     # return HttpResponse("欢迎%s"%(user.username))
-    return render(request, 'killer/killer.html')
+    list = ["GPS","屏","指纹","性能","相机","通话","电池","wifi","蓝牙"]
+    return render(request, 'killer/killer.html',{"username":user,"module_list":list})
